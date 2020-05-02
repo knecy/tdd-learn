ee7ae28bf8ccdb0aff6e7d2b34f96c886562aa98
liujun <liuj@shgbit.com>
2020-05-03 00:31:05 +0800

    fix test package

--- src/test/java/com/jiker/keju/controller/TaxiValuationControllerTest.java
+++ src/test/java/com/jiker/keju/controller/TaxiValuationControllerTest.java
@@ -1,13 +1,13 @@
 package com.jiker.keju.controller;
 
 import org.junit.Assert;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-class TaxiValuationControllerTest {
+public class TaxiValuationControllerTest {
     private TaxiValuationController taxiValuationController = new TaxiValuationController();
 
     @Test
-    void taxiValuation() {
+    public void taxiValuation() {
         Assert.assertEquals(taxiValuationController.taxiValuation(
                 "1公里,等待0分钟\\n" +
                 "3公里,等待0分钟\\n" +
@@ -20,19 +20,19 @@ class TaxiValuationControllerTest {
     }
 
     @Test
-    void calculateFee() {
+    public void calculateFee() {
         Assert.assertEquals(taxiValuationController.calculateFee("5公里,等待3分钟"), "9");
         Assert.assertNotEquals(taxiValuationController.calculateFee("5公里,等待3分钟"), "11.00");
     }
 
     @Test
-    void mileageFee() {
+    public void mileageFee() {
         Assert.assertEquals(taxiValuationController.mileageFee("10"), "13.20");
         Assert.assertNotEquals(taxiValuationController.mileageFee("7"), "11.00");
     }
 
     @Test
-    void waitingFee() {
+    public void waitingFee() {
         Assert.assertEquals(taxiValuationController.waitingFee("10"), "2.50");
         Assert.assertNotEquals(taxiValuationController.waitingFee("7"), "2.50");
     }
